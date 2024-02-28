eb1d265b5dd6f7d8c39f23d8a974a86a4ce7adfe
Villafruela <n.villafruela@fablab-moebius.org>
2024-02-28 12:19:08 +0100

    Corrections Code
    Fichier de détection de ligne, correction de code : valeurs des LED pour détection de ligne

--- Code Sumo Bot/C/detection-ligne/detection-ligne.ino
+++ Code Sumo Bot/C/detection-ligne/detection-ligne.ino
@@ -5,6 +5,7 @@
 *       Programme pour calibrer les capteur de détection de ligne
 *
 * NOTES :
+* Sumobot board Version 5.2
 *
 * AUTEUR :  Naimeric Villafruela
 * DATE DE CREATION :  10/09/2023
@@ -14,22 +15,15 @@
 * 10/09/2023  Naimeric Villafruela    Ecriture du code initial
 **/
 
-// Création des variables pour les détecteurs de ligne avant
-#define AV_DROIT 1
-#define AV_GAUCHE 2
 
 // déclaration des cateurs droits et gauches
-const int TCRT_G = A0;
-const int TCRT_D = A1;
+const int TCRT_G = A0; //A0
+const int TCRT_D = A1; //A1
 
 // Seuil de sensibilité des capteurs de ligne avant (à Régler en fonction du Dohyo)
+
 #define SEUIL_DROIT 800
 #define SEUIL_GAUCHE 800
-
-// Déclaration des numéros de pin
-#define D7 7 // LED GAUCHE
-#define D8 8 // LED DROITE
-
 
 /*
  * Pins des LEDs du sumobot
@@ -37,16 +31,24 @@ const int TCRT_D = A1;
  * notes: les LEDs peuvent vous indiquer en combat des informations diverses
  * selon l'endroit d'où vous les appelez dans votre programme
  */
-const int LED_G = D7;
-const int LED_D = D8;
+// Déclaration des numéros de pin
+#define LED_G 7 // LED GAUCHE
+#define LED_D 8 // LED DROITE
 
 /**
  * La fonction `setup()` est appelée lorsqu'un sketch démarre et ne sera exécutée qu'une seule fois, après chaque mise sous tension ou réinitialisation de la carte Arduino
  * On l'utilise pour initialiser notre programme en définissant la direction des pins ainsi que d'autres actions.
  */
 void setup() {
+
+  pinMode (LED_G, OUTPUT);
+  pinMode (LED_D, OUTPUT);
+  pinMode (TCRT_G, INPUT);
+  pinMode (TCRT_D, INPUT);
+
   Serial.begin(9600);
   Serial.println("Programme démarré");
+
 }
 
 /*
@@ -60,17 +62,19 @@ void loop() {
   Serial.print(CGAUCHE);
   Serial.print(" | ");
   Serial.println(CDROIT);
-  if (CGAUCHE <= SEUIL_GAUCHE) {
-    digitalWrite(LED_G, LOW);
-  } else {
-    digitalWrite(LED_G, HIGH);
-    Serial.println("allumé G");
-  }
-  if (CDROIT <= SEUIL_DROIT) {
-    digitalWrite(LED_D, LOW);
-  } else {
-    digitalWrite(LED_D, HIGH);
-    Serial.println("allumé D");
-  }
+  
+    if (CGAUCHE <= SEUIL_GAUCHE) {
+      digitalWrite(LED_G, LOW);
+    } else {
+      digitalWrite(LED_G, HIGH);
+      Serial.println("allumé G");
+    }
+    if (CDROIT <= SEUIL_DROIT) {
+      digitalWrite(LED_D, LOW);
+    } else {
+      digitalWrite(LED_D, HIGH);
+      Serial.println("allumé D");
+    }
+
   delay(500);
 }
